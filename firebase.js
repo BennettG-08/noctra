3418f857a05631363851387bd8c0d8049f6cd117
BennettG-08 <bennett.lg08@gmail.com>
2026-07-01 23:54:29 -0600

    Add Firebase initialization and Firestore import

--- firebase.js
+++ firebase.js
@@ -1,1 +1,9 @@
+// ==========================================================
+// NOCTRA - Firebase
+// ==========================================================
 
+// Importar Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
