538eba1e9297f71c2b7a1fc5bf1b92edd2dca496
BennettG-08 <bennett.lg08@gmail.com>
2026-07-03 02:39:14 -0600

    Export app along with db from firebase.js

--- firebase.js
+++ firebase.js
@@ -5,23 +5,16 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyDavM_1KqDWtuM5t88lrQnp3spnZxi-8GM",
-
   authDomain: "noctra-bbdaf.firebaseapp.com",
-
   projectId: "noctra-bbdaf",
-
   storageBucket: "noctra-bbdaf.firebasestorage.app",
-
   messagingSenderId: "1047689936282",
-
   appId: "1:1047689936282:web:bc83f1c390256821b3ce23"
-
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-export { db };
+export { app, db };
